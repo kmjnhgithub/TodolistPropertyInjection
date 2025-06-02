557c6d126cc25cf38e7fdb24496857b992d0d5a2
Mike <kmjnh2016@gmail.com>
2025-06-02 15:51:59 +0800

    Initial Commit

---
+++ TodolistPropertyInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodolistPropertyInjection
+//
+//  Created by mike liu on 2025/6/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
